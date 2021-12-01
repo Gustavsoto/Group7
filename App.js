b20ec7ca69b22d53846791a70db9270d1710fc18
Gustavs Oto Cers <gustavs.oto.cers@gmail.com>
2021-12-01 22:00:00 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello World from Group 7 Gustavs Oto Cers</Text>
+      <text>This is my Gustavs Oto Cers first React Native application</text>
+    </View>
+  )
+}
+export default HelloWorldApp;
